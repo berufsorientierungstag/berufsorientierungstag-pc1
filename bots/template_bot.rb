96c877663678893c1ac1476070825d5cdbc37f99
Berufsorientierung-1 <Berufsorientierung-1@invision.de>
2017-04-05 13:54:25 +0200

    Automatical

--- bots/template_bot.rb
+++ bots/template_bot.rb
@@ -1,1 +1,17 @@
-
+wiederholen
+if vorne_frei?
+	vor
+	drehe_rechts
+	if vorne_frei?
+		vor
+	end
+	drehe_links
+	vor
+end
+if !vorne_frei?
+	drehe_rechts
+	if !vorne_frei
+		drehe_rechts
+		drehe_rechts
+	end
+end
